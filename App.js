d6e45e833433ce237c4ef643923d8b167ea2be51
Hasina <anisahrab043@gmail.com>
2024-08-27 11:33:40 +0200

    Rice-eat + serveur flask = erreur

--- App.js
+++ App.js
@@ -5,15 +5,21 @@ import axios from 'axios';
 const App = () => {
   const [legumes, setLegumes] = useState([]);
   const [fruits, setFruits] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-    axios.get('http://192.168.68.68:5000/api/legumes')
+    axios.get('http://10.0.2.2:5000/api/legumes')
       .then(response => setLegumes(response.data))
-      .catch(error => console.error('Error fetching legumes:', error));
+      .catch(error => console.error('Error fetching legumes:', error)) ;
 
-    axios.get('http://192.168.68.68:5000/api/fruits')
+    axios.get('http://10.0.2.2:5000/api/fruits')
       .then(response => setFruits(response.data))
       .catch(error => console.error('Error fetching fruits:', error));
+
+    axios.get('http://10.0.2.2:5000/users')
+    .then(response => setUsers(response.data))
+    .catch(error => console.error('Error fetching users:', error));
   }, []);
 
   return (
@@ -31,6 +37,13 @@ const App = () => {
         keyExtractor={item => item.id.toString()}
         renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
       />
+      <Text style={styles.title}>Liste des Users</Text>
+      <FlatList
+        data={users}
+        // keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) => <Text style={styles.item}>{item.email}</Text>}
+      />
+      <Text style={styles.container}>{data ? data : "Loading..."}</Text>
     </View>
   );
 };
@@ -39,17 +52,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 40,
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20
+    marginBottom: 20,
   },
   item: {
     fontSize: 18,
-    marginBottom: 10
-  }
+    marginBottom: 10,
+  },
 });
 
 export default App;
--- app.py
+++ app.py
@@ -1,32 +1,122 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from database import db
-from database.legumes import legume_blueprint
-from database.fruits import fruit_blueprint
-from routes.route import test_bp
-from routes.translation_route import translator_bp
+from flask_marshmallow import Marshmallow
+from flask_mail import Mail, Message
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+from random import *
 
 app = Flask(__name__)
-
 CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'anIsah27$'
+app.config['JWT_SECRET_KEY'] = 'anIsah27$'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'anisahrab043@gmail.com'
+app.config['MAIL_PASSWORD'] = 'hasina28$'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 
-db.init_app(app)
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+mail = Mail(app)
+jwt = JWTManager(app)
 
-app.register_blueprint(legume_blueprint, url_prefix='/api')
-app.register_blueprint(fruit_blueprint, url_prefix='/api')
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=True)
 
-app.register_blueprint(test_bp)
-app.register_blueprint(translator_bp)
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
 
-@app.before_request
-def create_tables():
-    db.create_all()
-    # Vérifiez que les tables sont créées
-    # print(db.engine.table_names())
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'email', 'password')
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    all_users = User.query.all()
+    results = users_schema.dump(all_users)
+    return jsonify(results)
+
+@app.route('/users/<id>/', methods=['GET'])
+def post_details(id):
+    user = User.query.get(id)
+    return user_schema.jsonify(user)
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    email = request.json['email']
+    password = request.json['password']
+    users = User(email, password)
+    db.session.add(users)
+    db.session.commit()
+    return user_schema.jsonify(users)
+
+@app.route('/update/<id>/', methods=['PUT'])
+def update_user(id):
+    user = User.query.get(id)
+    email = request.json['email']
+    password = request.json['password']
+    user.email = email
+    user.password = password
+
+    db.session.commit()
+    return user_schema.jsonify(user)
+
+@app.route('/delete/<id>/', methods=['DELETE'])
+def delete_user(id):
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+    return user_schema.jsonify(user)
+
+@app.route('/reset_password', methods=['POST'])
+def reset_password():
+    # if request.content_type != 'application/json':
+    #     return jsonify({'message': 'Content-Type must be application/json'}), 400
+
+    data = request.get_json()
+    user = User.query.filter_by(email=data['email']).first()
+
+    if not user:
+        return jsonify({'message': 'Email does not exist'}), 404
+
+    new_password = 'temporary_password'
+    user.password = generate_password_hash(new_password, method='sha256')
+    db.session.commit()
+
+    msg = Message('Password Reset Request',
+                  sender='anisahrab043@gmail.com',
+                  recipients=[user.email])
+    msg.body = f'Your new temporary password is: {new_password}'
+    mail.send(msg)
+
+    return jsonify({'message': 'A new password has been sent to your email.'}), 200
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/api/legumes", methods=['GET'])
+def get_legumes():
+    return jsonify([
+        {"id": 1, "name": "Carotte"},
+        {"id": 2, "name": "Tomate"},
+    ])
+
+@app.route("/api/fruits", methods=['GET'])
+def get_fruits():
+    return jsonify([
+        {"id": 1, "name": "Pomme"},
+        {"id": 2, "name": "Banane"},
+    ])
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
